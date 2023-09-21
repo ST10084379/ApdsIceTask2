825b82f90487a03a5224fce4a2cf8242d223d9d9
ST10084379 <st10084379@vcconnect.edu.za>
2023-09-21 10:01:05 +0200

    Added users and Login

--- server/server.mjs
+++ server/server.mjs
@@ -17,6 +17,7 @@ const options = {
   }
 
 import records from "./routes/record.mjs";
+import users from "./routes/user.mjs"
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -24,13 +25,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/record", records);
+app.use("/user", users);
 
 let server = https.createServer(options,app)
 
-app.get('/',(req,res)=>{
-  res.send('HTTPS in ExpressJS')
+app.get('/record',(req,res)=>{
+  //res.send('HTTPS in ExpressJS YASSSSSS')
 })
 
 //start the Express server
